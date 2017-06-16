e569594fdac290d94571b03d41e457be12a22ba7
Cameron Presley <cameron@thesoftwarementor.com>
2017-06-15 21:13:14 -0400

    Added the option class with Some, None, Map, and AndThen

---
+++ Optionally/Option.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Optionally
+{
+    public class Option<T>
+    {
+        private readonly T _value;
+        private readonly bool _hasValue;
+
+        private Option(T value, bool hasValue)
+        {
+            _value = value;
+            _hasValue = hasValue;
+        }
+
+        public static Option<T> Some(T value)
+        {
+            return new Option<T>(value, true);
+        }
+
+        public static Option<T> None()
+        {
+            return new Option<T>(default(T), false);
+        }
+
+        public Option<U> Map<U>(Func<T, U> mapper)
+        {
+            return _hasValue ? new Option<U>(mapper(_value), true) : new Option<U>(default(U), false);
+        }
+
+        public Option<U> AndThen<U>(Func<T, Option<U>> chain)
+        {
+            return _hasValue ? chain(_value) : new Option<U>(default(U), false);
+        }
+    }
+}
