6983da777b5d9fe1ab5f46efe74399e518546645
Cameron Presley <cameron@thesoftwarementor.com>
2017-06-15 21:17:33 -0400

    Updated project reference, also added null check for IEnumerableExtensions.

--- Optionally/IEnumerableExtensions.cs
+++ Optionally/IEnumerableExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Optionally
 {
@@ -10,6 +8,7 @@ namespace Optionally
     {
         public static Option<T> TryHead<T>(this IEnumerable<T> enumerable)
         {
+            if (enumerable == null) return Option<T>.None();
             foreach (var v in enumerable)
             {
                 return Option<T>.Some(v);
@@ -19,6 +18,7 @@ namespace Optionally
 
         public static Option<T> TryHead<T>(this IEnumerable<T> enumerable, Func<T, bool> filter)
         {
+            if (enumerable == null) return Option<T>.None();
             return enumerable.Where(filter).TryHead();
         }
     }
