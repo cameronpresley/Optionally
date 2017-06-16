417f36004cca02b3a1b7d1cd7a6fdd044f4b77b6
Cameron Presley <cameron@thesoftwarementor.com>
2017-06-15 21:16:08 -0400

    Added IEnumerableExtensions class with TryHead and TryHead filter.

---
+++ Optionally/IEnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optionally
+{
+    public static class IEnumerableExtensions
+    {
+        public static Option<T> TryHead<T>(this IEnumerable<T> enumerable)
+        {
+            foreach (var v in enumerable)
+            {
+                return Option<T>.Some(v);
+            }
+            return Option<T>.None();
+        }
+
+        public static Option<T> TryHead<T>(this IEnumerable<T> enumerable, Func<T, bool> filter)
+        {
+            return enumerable.Where(filter).TryHead();
+        }
+    }
+}
