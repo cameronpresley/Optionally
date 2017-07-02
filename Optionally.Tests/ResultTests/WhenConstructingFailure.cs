d054cbeccf10b0b5afdcfdd0e790effd4ced729b
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-02 11:19:55 -0400

    Added test for when constructing Failures

---
+++ Optionally.Tests/ResultTests/WhenConstructingFailure.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Optionally.Tests.ResultTests
+{
+    [TestFixture]
+    class WhenConstructingFailure
+    {
+        [Test]
+        public void AndTheValueIsNullThenFailureIsReturned()
+        {
+            var observed = Result<int, string>.Failure(null);
+
+            var expected = Result<int, string>.Failure(null);
+            Assert.AreEqual(expected, observed);
+        }
+
+        [Test]
+        public void AndTheValueIsNotNullThenFailureIsReturned()
+        {
+            var observed = Result<int, string>.Failure("Whoops");
+
+            var expected = Result<int, string>.Failure("Whoops");
+            Assert.AreEqual(expected, observed);
+        }
+    }
+}
