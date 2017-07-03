b5e9560e0f55674f0eddc6add066030d0c105470
Cameron Presley <cameron@thesoftwarementor.com>
2017-07-02 20:53:38 -0400

    Added tests around IEnumerableExtensions (#2)
    * Added tests for when retrieving the first element of the list.
    
    * Added tests for when retrieving the first element of a filtered list.

---
+++ Optionally.Tests/IEnumerableExtensionTests/WhenRetrievingFirst.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Optionally.Tests.IEnumerableExtensionTests
+{
+    [TestFixture]
+    class WhenRetrievingFirst
+    {
+        [Test]
+        public void AndTheListIsNullThenNoneIsReturned()
+        {
+            var observed = ((List<int>)null).TryFirst();
+
+            var expected = Option<int>.None();
+            Assert.AreEqual(expected, observed);
+        }
+
+        [Test]
+        public void AndTheListHasNoElementsThenNoneIsReturned()
+        {
+            var observed = new List<int>().TryFirst();
+
+            var expected = Option<int>.None();
+            Assert.AreEqual(expected, observed);
+        }
+
+        [Test]
+        public void AndTheListHasASingleElementThenSomeIsReturned()
+        {
+            var element = 2;
+
+            var observed = new List<int> { element }.TryFirst();
+
+            var expected = Option<int>.Some(element);
+            Assert.AreEqual(expected, observed);
+        }
+
+        [Test]
+        public void AndTheListHasMultipleElementsThenTheFirstIsReturnedAsSome()
+        {
+            var firstElement = 2;
+
+            var observed = new List<int> { firstElement, 5, -10 }.TryFirst();
+
+            var expected = Option<int>.Some(firstElement);
+            Assert.AreEqual(expected, observed);
+        }
+    }
+}
