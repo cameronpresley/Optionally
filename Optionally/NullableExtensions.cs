6ddf0f1e6e9fc8ff12c5bb7d2d879ecf54941037
Cameron Presley <cameron@thesoftwarementor.com>
2017-07-03 18:23:07 -0400

    Nullable extensions (#6)
    * Added implementation for Nullable.ToOption
    
    * Added tests for Nullable<T>.ToOption(), also added logic if the Nullable instance was null.

---
+++ Optionally/NullableExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Optionally
+{
+    public static class NullableExtensions
+    {
+        /// <summary>
+        /// Converts a Nullable to an Option
+        /// </summary>
+        /// <typeparam name="T">Type of the nullable</typeparam>
+        /// <param name="nullable"></param>
+        /// <returns>If nullable is null or has no value, then None. Otherwise Some</returns>
+        public static Option<T> ToOption<T>(this T? nullable) where T : struct
+        {
+            if (nullable == null) return Option<T>.None();
+            if (!nullable.HasValue) return Option<T>.None();
+            return Option<T>.Some(nullable.Value);
+        }
+    }
+}
