1a29adc2e9820796d1871eb73d4dc800a1cc04fe
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-02 20:51:52 -0400

    Added tests for when retrieving the first element of a filtered list.

---
+++ Optionally.Tests/IEnumerableExtensionTests/WhenRetrievingFirstByFilter.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Optionally.Tests.IEnumerableExtensionTests
+{
+    [TestFixture]
+    class WhenRetrievingFirstByFilter
+    {
+        [Test]
+        public void AndTheListIsNullThenNoneIsReturned()
+        {
+            var observed = ((List<int>)null).TryFirst(_ => true);
+
+            var expected = Option<int>.None();
+            Assert.AreEqual(expected, observed);
+        }
+        
+        [Test]
+        public void AndTheFilterIsNullThenNoneIsReturned()
+        {
+            var observed = new List<int> { 2 }.TryFirst(null);
+
+            var expected = Option<int>.None();
+            Assert.AreEqual(expected, observed);
+        }
+
+        [Test]
+        public void AndTheListIsEmptyThenNoneIsReturned()
+        {
+            var observed = new List<int>().TryFirst(_ => true);
+
+            var expected = Option<int>.None();
+            Assert.AreEqual(expected, observed);
+        }
+
+        [Test]
+        public void AndTheFilterReturnsNoResultsThenNoneIsReturned()
+        {
+            var observed = new List<int> { 1, 2, 3 }.TryFirst(_ => false);
+
+            var expected = Option<int>.None();
+            Assert.AreEqual(expected, observed);
+        }
+
+        [Test]
+        public void AndTheFilterReturnsMultipleResultsThenTheFirstIsReturnedAsSome()
+        {
+            var observed = new List<int> { 1, 2, 3 }.TryFirst(_ => true);
+
+            var expected = Option<int>.Some(1);
+            Assert.AreEqual(expected, observed);
+        }
+    }
+}
