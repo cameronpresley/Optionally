f87559e768fe7c903274fd42e2cb2d703cfa14c6
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-02 21:27:26 -0400

    Added documentation for Option class.

--- Optionally/Option.cs
+++ Optionally/Option.cs
@@ -2,6 +2,10 @@
 
 namespace Optionally
 {
+    /// <summary>
+    /// Represents whether a value exists or not
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Option<T>
     {
         private readonly T _value;
@@ -13,16 +17,33 @@ namespace Optionally
             _hasValue = hasValue;
         }
 
+        /// <summary>
+        /// Factory method that creates an Option with a value inside
+        /// </summary>
+        /// <param name="value">The value to embed in the Option</param>
+        /// <remarks>Does not check if value is null</remarks>
+        /// <returns></returns>
         public static Option<T> Some(T value)
         {
             return new Option<T>(value, true);
         }
 
+        /// <summary>
+        /// Factory method that creates an Option without a value
+        /// </summary>
+        /// <returns></returns>
         public static Option<T> None()
         {
             return new Option<T>(default(T), false);
         }
 
+        /// <summary>
+        /// Convert the current Option to a different Option
+        /// </summary>
+        /// <typeparam name="U">Type to convert to</typeparam>
+        /// <param name="mapper">Function to convert T to U</param>
+        /// <returns>Some if Option is a Some, None otherwise</returns>
+        /// <remarks>Similiar to Select from LINQ</remarks>
         public Option<U> Map<U>(Func<T, U> mapper)
         {
             if (mapper == null) return Option<U>.None();
@@ -31,6 +52,13 @@ namespace Optionally
                 : Option<U>.None();
         }
 
+        /// <summary>
+        /// Chain a function call using the current Option
+        /// </summary>
+        /// <typeparam name="U">Type of Option to return</typeparam>
+        /// <param name="binder">Function to call if Option is Some</param>
+        /// <returns>If Option is Some, returns the result of binder(value), otherwise, None </returns>
+        /// <remarks>Provides a monadic approach to data validation</remarks>
         public Option<U> AndThen<U>(Func<T, Option<U>> binder)
         {
             if (binder == null) return Option<U>.None();
@@ -39,6 +67,11 @@ namespace Optionally
                 : Option<U>.None();
         }
 
+        /// <summary>
+        /// Performs an Action on the current Option
+        /// </summary>
+        /// <param name="ifSome">Function to call if Option is Some</param>
+        /// <param name="ifNone">Function to call if Option is None</param>
         public void Do(Action<T> ifSome, Action ifNone)
         {
             if (_hasValue && ifSome != null)
@@ -51,6 +84,11 @@ namespace Optionally
             }
         }
 
+        /// <summary>
+        /// Check if Option fulfills the filter
+        /// </summary>
+        /// <param name="filter">Predicate used to test Option</param>
+        /// <returns>If Option is Some and the value fulfills the filter, then Some. Otherwise None</returns>
         public Option<T> Where(Func<T, bool> filter)
         {
             if (filter == null) return None();
@@ -58,6 +96,16 @@ namespace Optionally
             return None();
         }
 
+        /// <summary>
+        /// Create an option based on a function call and Option arguments
+        /// </summary>
+        /// <typeparam name="T1">Type of the first arugment for the func</typeparam>
+        /// <typeparam name="T2">Type of the second argument for the func</typeparam>
+        /// <param name="func">Function to call if all the arguments are Some</param>
+        /// <param name="first">First argument for func</param>
+        /// <param name="second">Second argument for the func</param>
+        /// <returns>If all arguments are Some, then Some is returned. None otherwise</returns>
+        /// <remarks>Provides an applicative style for data validation</remarks>
         public static Option<T> Apply<T1, T2>(Func<T1, T2, T> func, Option<T1> first, Option<T2> second)
         {
             if (func == null || first == null || second == null) return None();
@@ -67,6 +115,18 @@ namespace Optionally
             return None();
         }
 
+        /// <summary>
+        /// Create an option based on a function call and Option arguments
+        /// </summary>
+        /// <typeparam name="T1">Type of the first argument for the func</typeparam>
+        /// <typeparam name="T2">Type of the second argument for the func</typeparam>
+        /// <typeparam name="T3">Type of the third argument for the func</typeparam>
+        /// <param name="func">Function to call if all the arguments are Some</param>
+        /// <param name="first">First argument for func</param>
+        /// <param name="second">Second argument for func</param>
+        /// <param name="third">Third argument for func</param>
+        /// <returns>If all arguments are Some, then Some is returned. Otherwise, None is returned</returns>
+        /// <remarks>Provides an applicative style for data validation</remarks>
         public static Option<T> Apply<T1, T2, T3>(Func<T1, T2, T3, T> func, Option<T1> first, Option<T2> second, Option<T3> third)
         {
             if (func == null || first == null || second == null || third == null) return None();
