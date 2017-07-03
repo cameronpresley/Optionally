0c81f4c7f1eabfd6eb2d8663eebf3394b480f660
Cameron Presley <cameron@thesoftwarementor.com>
2017-07-02 21:44:38 -0400

    Result documentation (#5)
    * Added docs to both TryFirst and TryFirst(filter) extension methods.
    
    * Added documentation for Option class.
    
    * Added documentation on Result.

--- Optionally/Result.cs
+++ Optionally/Result.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 
 namespace Optionally
 {
+    /// <summary>
+    /// Models a Success or a Failure
+    /// </summary>
+    /// <typeparam name="T">Type of success</typeparam>
+    /// <typeparam name="U">Type of failure</typeparam>
     public class Result<T, U>
     {
         private readonly T _success;
@@ -16,16 +21,34 @@ namespace Optionally
             _didSucceed = didSucceed;
         }
 
+        /// <summary>
+        /// Creates a Success Result with the value
+        /// </summary>
+        /// <param name="value">Value of the success</param>
+        /// <returns>Success Result</returns>
+        /// <remarks>Does not check if value is null</remarks>
         public static Result<T, U> Success(T value)
         {
             return new Result<T, U>(value, default(U), true);
         }
 
+        /// <summary>
+        /// Creates a Failure result with the value
+        /// </summary>
+        /// <param name="value">Value of the failure</param>
+        /// <returns>Failure Result</returns>
+        /// <remarks>Does not check if value is null</remarks>
         public static Result<T, U> Failure(U value)
         {
             return new Result<T, U>(default(T), value, false);
         }
 
+        /// <summary>
+        /// Converts a Success Result to another Success Result
+        /// </summary>
+        /// <typeparam name="V">Type of the new Success</typeparam>
+        /// <param name="mapper">How to convert the Success value</param>
+        /// <returns>If Result is a Success, then Success is returned. Otherwise a Failure is returned</returns>
         public Result<V, U> Map<V>(Func<T, V> mapper)
         {
             if (mapper == null) throw new ArgumentNullException(nameof(mapper));
@@ -34,12 +57,24 @@ namespace Optionally
                 : Result<V, U>.Failure(_failure);
         }
 
+        /// <summary>
+        /// Chains a Success Result with a function call
+        /// </summary>
+        /// <typeparam name="V">Type of the Result from binder</typeparam>
+        /// <param name="binder">Function to call if current Result is a Success</param>
+        /// <returns>If Result is a Success, then binder is called with the success value. Otherwise, None is returned</returns>
+        /// <remarks>Provides a monadic approach to data validation</remarks>
         public Result<V, U> AndThen<V>(Func<T, Result<V, U>> binder)
         {
             if (binder == null) throw new ArgumentNullException(nameof(binder));
             return _didSucceed ? binder(_success) : Result<V, U>.Failure(_failure);
         }
 
+        /// <summary>
+        /// Perform an action on the current Result
+        /// </summary>
+        /// <param name="onSuccess">Action to call if Result is a Success</param>
+        /// <param name="onFailure">Action to call if Result is a Failure</param>
         public void Do(Action<T> onSuccess, Action<U> onFailure)
         {
             if (_didSucceed && onSuccess != null)
@@ -48,6 +83,17 @@ namespace Optionally
                 onFailure(_failure);
         }
 
+        /// <summary>
+        /// Apply a function to a series of Result arguments
+        /// </summary>
+        /// <typeparam name="T1">Type of the first argument</typeparam>
+        /// <typeparam name="T2">Type of the second argument</typeparam>
+        /// <param name="func">Function to call if all arguments are a Success</param>
+        /// <param name="first">First argument for the function</param>
+        /// <param name="second">Second argument for the function</param>
+        /// <returns>If all arguments are Success, then Success is returned. Otherwise, a Failure with all the argument Failures is returned</returns>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        /// <remarks>Provides an applicative style of data validation</remarks>
         public static Result<T, List<U>> Apply<T1, T2>(Func<T1, T2, T> func, Result<T1, U> first, Result<T2, U> second)
         {
             if (func == null) throw new ArgumentNullException(nameof(func));
@@ -63,6 +109,18 @@ namespace Optionally
             return Result<T, List<U>>.Failure(errors);
         }
 
+        /// <summary>
+        /// Applies a function to a series of Result arguments
+        /// </summary>
+        /// <typeparam name="T1">Type of the first argument</typeparam>
+        /// <typeparam name="T2">Type of the second argument</typeparam>
+        /// <typeparam name="T3">Type of the third argument</typeparam>
+        /// <param name="func">Function to call</param>
+        /// <param name="first">First argument for the function</param>
+        /// <param name="second">Second argument for the function</param>
+        /// <param name="third">Third argument for the function</param>
+        /// <returns>If all arguments are Success, then a Success is returned. Otherwise, all the Failures are concatentated into a Failure Result</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static Result<T, List<U>> Apply<T1, T2, T3>(Func<T1, T2, T3, T> func, Result<T1, U> first, Result<T2, U> second, Result<T3, U> third)
         {
             if (func == null) throw new ArgumentNullException(nameof(func));
