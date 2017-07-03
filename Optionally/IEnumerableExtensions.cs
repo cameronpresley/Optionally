3d56646c8b6131a9703f5d653819a607d45cedaa
Cameron Presley <cameron@thesoftwarementor.com>
2017-07-02 21:43:51 -0400

    Added docs to both TryFirst and TryFirst(filter) extension methods. (#3)

--- Optionally/IEnumerableExtensions.cs
+++ Optionally/IEnumerableExtensions.cs
@@ -6,6 +6,12 @@ namespace Optionally
 {
     public static class IEnumerableExtensions
     {
+        /// <summary>
+        /// Attemps to retrieve the first element in an IEnumerable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable"></param>
+        /// <returns>None if the list is empty or null, Some(T) otherwise</returns>
         public static Option<T> TryFirst<T>(this IEnumerable<T> enumerable)
         {
             if (enumerable == null) return Option<T>.None();
@@ -16,6 +22,14 @@ namespace Optionally
             return Option<T>.None();
         }
 
+
+        /// <summary>
+        /// Attemps to retrieve the first element in an IEnumerable that satisifies the filter function
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable"></param>
+        /// <param name="filter"></param>
+        /// <returns>None if the enumerable or filter is null. None if filter produces an empty IEnumerable, otherwise Some(T)</returns>
         public static Option<T> TryFirst<T>(this IEnumerable<T> enumerable, Func<T, bool> filter)
         {
             if (enumerable == null) return Option<T>.None();
