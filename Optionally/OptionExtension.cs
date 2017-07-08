cb8278a1f500366084834b01999d2219c62df840
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-08 07:44:54 -0400

    Added the comments to Apply for the static Option class.

--- Optionally/OptionExtension.cs
+++ Optionally/OptionExtension.cs
@@ -4,6 +4,17 @@ namespace Optionally
 {
     public static class Option
     {
+        /// <summary>
+        /// Call a function with Option inputs 
+        /// </summary>
+        /// <typeparam name="T1">Type of the first argument for the function</typeparam>
+        /// <typeparam name="T2">Type of the second argument for the function</typeparam>
+        /// <typeparam name="TResult">Type of the result of the function</typeparam>
+        /// <param name="func"></param>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns>Some(TResult) if first and second are Some. Otherwise, None</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static Option<TResult> Apply<T1, T2, TResult>(Func<T1, T2, TResult> func, Option<T1> first, Option<T2> second)
         {
             if (func == null) throw new ArgumentNullException(nameof(func));
@@ -11,6 +22,18 @@ namespace Optionally
             return Option<TResult>.None;
         }
 
+        /// <summary>
+        /// Call a function with Option inputs
+        /// </summary>
+        /// <typeparam name="T1">Type of the first argument for function</typeparam>
+        /// <typeparam name="T2">Type of the second argument for function</typeparam>
+        /// <typeparam name="T3">Type of the third argument for function</typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="func"></param>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="third"></param>
+        /// <returns>Some(TResult) if first, second, and third are Some. Otherwise, None</returns>
         public static Option<TResult> Apply<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> func, Option<T1> first,
             Option<T2> second, Option<T3> third)
         {
