1d99cf3c94032fae669e7713f595f958d2bd2759
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-08 22:07:00 -0400

    Added tests for when converting to a bool.

---
+++ Optionally.Tests/ConverterTests/WhenConvertingToABool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Optionally.Tests.ConverterTests
+{
+    [TestFixture]
+    public class WhenConvertingToABool
+    {
+        [Test]
+        public void AndInputIsTrueThenSomeIsReturned()
+        {
+            var result = Converter.ToBool("true");
+
+            var expected = Option.Some(true);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void AndInputIs0ThenNoneIsReturned()
+        {
+            var result = Converter.ToBool("0");
+
+            var expected = Option.No<bool>();
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void AndInputIsWordThenNoneIsReturned()
+        {
+            var result = Converter.ToBool("word");
+
+            var expected = Option.No<bool>();
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
