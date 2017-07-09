4f5b174148b47c9bd66ec62d607531cc1c8b5495
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-08 22:10:22 -0400

    Added tests for when converting to a decimal.

---
+++ Optionally.Tests/ConverterTests/WhenConvertingToADecimal.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Optionally.Tests.ConverterTests
+{
+    [TestFixture]
+    public class WhenConvertingToADecimal
+    {
+        [Test]
+        public void AndInputIsANumberThenSomeIsReturned()
+        {
+            var result = Converter.ToDecimal("12.34");
+
+            var expected = Option.Some(12.34m);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void AndInputIsAWholeNumberThenSomeIsReturned()
+        {
+            var result = Converter.ToDecimal("12");
+
+            var expected = Option.Some(12m);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void AndInputIsNotANumberThenNoneIsReturned()
+        {
+            var result = Converter.ToDecimal("something");
+
+            var expected = Option.No<decimal>();
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
