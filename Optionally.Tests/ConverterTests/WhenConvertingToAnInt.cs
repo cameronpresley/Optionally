459e4a87561eb9955458845c3fbb8d9286f43bf3
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-08 21:59:20 -0400

    Added implementations, added test for converting integers.

---
+++ Optionally.Tests/ConverterTests/WhenConvertingToAnInt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Optionally.Tests.ConverterTests
+{
+    [TestFixture]
+    public class WhenConvertingToAnInt
+    {
+        [Test]
+        public void AndInputIsAnIntegerThenSomeIsReturned()
+        {
+            var result = Converter.ToInt("15");
+
+            var expected = Option.Some(15);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void AndInputIsADoubleThenNoneIsReturned()
+        {
+            var result = Converter.ToInt("15.2");
+            
+            var expected = Option.No<int>();
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void AndInputIsNotANumberThenNoneIsReturned()
+        {
+            var result = Converter.ToInt("kumquats");
+
+            var expected = Option.No<int>();
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Optionally/Converter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Optionally
+{
+    public static class Converter
+    {
+        public static Option<Int32> ToInt(string s)
+        {
+            return Int32.TryParse(s, out int x) ? Option.Some(x) : Option.No<int>();
+        }
+
+        public static Option<DateTime> ToDateTime(string s)
+        {
+            return DateTime.TryParse(s, out DateTime x) ? Option.Some(x) : Option.No<DateTime>();
+        }
+
+        public static Option<decimal> ToDecimal(string s)
+        {
+            return Decimal.TryParse(s, out decimal x) ? Option.Some(x) : Option.No<decimal>();
+        }
+
+        public static Option<double> ToDouble(string s)
+        {
+            return Double.TryParse(s, out double x) ? Option.Some(x) : Option.No<double>();
+        }
+
+        public static Option<bool> ToBool(string s)
+        {
+            return Boolean.TryParse(s, out bool x) ? Option.Some(x) : Option.No<bool>();
+        }
+    }
+}
