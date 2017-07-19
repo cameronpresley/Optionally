97e09e261040e1d8cf992ac872e563aab4bfdb98
Cameron Presley <cameron@thesoftwarementor.com>
2017-07-18 20:47:34 -0400

    Added ToString Override to Option and Result (#17)
    * Added ToString override for Option. Also added tests.
    
    * Added ToString override for Result. Also added tests.
    
    * Chaned ToString message for Some.

---
+++ Optionally.Tests/ResultTests/WhenConvertingToString.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Optionally.Tests.ResultTests
+{
+    [TestFixture]
+    public class WhenConvertingToString
+    {
+        [Test]
+        public void AndTheResultIsFailureThenFailureStringIsReturned()
+        {
+            var result = Result<int, string>.Failure("failed result");
+
+            Assert.AreEqual("Failure of 'failed result'", result.ToString());
+        }
+
+        [Test]
+        public void AndTheResultIsSuccessThenSuccessStringIsReturned()
+        {
+            var result = Result<int, string>.Success(10);
+
+            Assert.AreEqual("Success of '10'", result.ToString());
+        }
+    }
+}
