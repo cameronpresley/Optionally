95341dff4f9a88a775d58bf37ab20ebd129068d0
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-09-02 03:38:08 -0400

    Created IResult inteface with two internal structs, Success and Failure.

--- Optionally/Result.cs
+++ Optionally/Result.cs
@@ -2,59 +2,15 @@
 
 namespace Optionally
 {
-    /// <summary>
-    /// Models a Success or a Failure
-    /// </summary>
-    /// <typeparam name="TFailure">Type of failure</typeparam>
-    /// <typeparam name="TSuccess">Type of success</typeparam>
-    public struct Result<TFailure, TSuccess>
+    public interface IResult<TFailure, TSuccess>
     {
-        internal readonly TFailure FailureValue;
-        internal readonly TSuccess SuccessValue;
-        internal readonly bool DidSucceed;
-
-        private Result(TFailure failure, TSuccess success, bool didSucceed)
-        {
-            FailureValue = failure;
-            SuccessValue = success;
-            DidSucceed = didSucceed;
-        }
-
-        /// <summary>
-        /// Creates a Success Result with the value
-        /// </summary>
-        /// <param name="value">Value of the success</param>
-        /// <returns>Success Result</returns>
-        /// <remarks>Does not check if value is null</remarks>
-        public static Result<TFailure, TSuccess> Success(TSuccess value)
-        {
-            return new Result<TFailure, TSuccess>(default(TFailure), value, true);
-        }
-
-        /// <summary>
-        /// Creates a Failure result with the value
-        /// </summary>
-        /// <param name="value">Value of the failure</param>
-        /// <returns>Failure Result</returns>
-        /// <remarks>Does not check if value is null</remarks>
-        public static Result<TFailure, TSuccess> Failure(TFailure value)
-        {
-            return new Result<TFailure, TSuccess>(value, default(TSuccess), false);
-        }
-
         /// <summary>
         /// Converts a Success Result to another Success Result
         /// </summary>
         /// <typeparam name="U">Type of the new Success</typeparam>
         /// <param name="mapper">How to convert the Success value</param>
         /// <returns>If Result is a Success, then Success is returned. Otherwise a Failure is returned</returns>
-        public Result<TFailure, U> Map<U>(Func<TSuccess, U> mapper)
-        {
-            if (mapper == null) throw new ArgumentNullException(nameof(mapper));
-            return DidSucceed
-                ? Result<TFailure, U>.Success(mapper(SuccessValue))
-                : Result<TFailure, U>.Failure(FailureValue);
-        }
+        IResult<TFailure, U> Map<U>(Func<TSuccess, U> mapper);
 
         /// <summary>
         /// Chains a Success Result with a function call
@@ -63,28 +19,72 @@ namespace Optionally
         /// <param name="binder">Function to call if current Result is a Success</param>
         /// <returns>If Result is a Success, then binder is called with the success value. Otherwise, None is returned</returns>
         /// <remarks>Provides a monadic approach to data validation</remarks>
-        public Result<TFailure, U> AndThen<U>(Func<TSuccess, Result<TFailure, U>> binder)
-        {
-            if (binder == null) throw new ArgumentNullException(nameof(binder));
-            return DidSucceed ? binder(SuccessValue) : Result<TFailure, U>.Failure(FailureValue);
-        }
+        IResult<TFailure, U> AndThen<U>(Func<TSuccess, IResult<TFailure, U>> binder);
 
         /// <summary>
         /// Perform an action on the current Result
         /// </summary>
         /// <param name="onSuccess">Action to call if Result is a Success</param>
         /// <param name="onFailure">Action to call if Result is a Failure</param>
-        public void Do(Action<TSuccess> onSuccess, Action<TFailure> onFailure)
+        void Do(Action<TSuccess> onSuccess, Action<TFailure> onFailure);
+    }
+
+    internal struct Success <TFailure, TSuccess> : IResult<TFailure, TSuccess>
+    {
+        internal readonly TSuccess Value;
+
+        public Success(TSuccess value)
         {
-            if (DidSucceed)
-                onSuccess?.Invoke(SuccessValue);
-            else
-                onFailure?.Invoke(FailureValue);
+            Value = value;
         }
 
-        public override string ToString()
+        public IResult<TFailure, U> Map<U>(Func<TSuccess, U> mapper)
         {
-            return DidSucceed ? $"Success of '{SuccessValue}'" : $"Failure of '{FailureValue}'";
+            if (mapper == null) throw new ArgumentNullException(nameof(mapper));
+            return new Success<TFailure, U>(mapper(Value));
+        }
+
+        public IResult<TFailure, U> AndThen<U>(Func<TSuccess, IResult<TFailure, U>> binder)
+        {
+            if (binder == null) throw new ArgumentNullException(nameof(binder));
+            return binder(Value);
+        }
+
+        public void Do(Action<TSuccess> onSuccess, Action<TFailure> onFailure)
+        {
+            if (onSuccess == null) throw new ArgumentNullException(nameof(onSuccess));
+            if (onFailure == null) throw new ArgumentNullException(nameof(onFailure));
+
+            onSuccess(Value);
+        }
+    }
+
+    internal struct Failure <TFailure, TSuccess> : IResult<TFailure, TSuccess>
+    {
+        internal readonly TFailure Value;
+
+        public Failure(TFailure value)
+        {
+            Value = value;
+        }
+
+        public IResult<TFailure, U> Map<U>(Func<TSuccess, U> mapper)
+        {
+            if (mapper == null) throw new ArgumentNullException(nameof(mapper));
+            return new Failure<TFailure, U>(Value);
+        }
+
+        public IResult<TFailure, U> AndThen<U>(Func<TSuccess, IResult<TFailure, U>> binder)
+        {
+            if (binder == null) throw new ArgumentNullException(nameof(binder));
+            return new Failure<TFailure, U>(Value);
+        }
+
+        public void Do(Action<TSuccess> onSuccess, Action<TFailure> onFailure)
+        {
+            if (onSuccess == null) throw new ArgumentNullException(nameof(onSuccess));
+            if (onFailure == null) throw new ArgumentNullException(nameof(onFailure));
+            onFailure(Value);
         }
     }
 }
