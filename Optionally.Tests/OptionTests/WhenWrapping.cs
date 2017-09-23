117c5c4624a8424a2c515514b7957a271662bbb6
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-09-23 09:25:00 -0400

    Added tests for Option.Wrap

--- Optionally.Tests/OptionTests/WhenWrapping.cs
+++ Optionally.Tests/OptionTests/WhenWrapping.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Optionally.Tests.OptionTests
 {
-    class WhenWrapping
+    [TestFixture]
+    public class WhenWrapping
     {
+        [Test]
+        public void AndTheFuncThrowsAnExceptionThenNoneIsReturned()
+        {
+            int GetNumber() => throw new Exception();
+
+            var observed = Option.Wrap(GetNumber);
+
+            var expected = Option.No<int>();
+            Assert.AreEqual(expected, observed);
+        }
+
+        [Test]
+        public void AndTheFuncReturnsAValueThenSomeValueIsReturned()
+        {
+            int GetNumber() => 2;
+
+            var observed = Option.Wrap(GetNumber);
+
+            var expected = Option.Some(2);
+            Assert.AreEqual(expected, observed);
+        }
+
+        [Test]
+        public void AndTheFuncIsNullThenNoneIsReturned()
+        {
+            Func<int> nullFunc = null;
+
+            var observed = Option.Wrap(nullFunc);
+
+            var expected = Option.No<int>();
+            Assert.AreEqual(expected, observed);
+        }
     }
 }
