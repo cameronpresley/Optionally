0582d7ca109e8a47d4ca15276ab5e1dcfe270976
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-09-26 22:16:42 -0400

    Fixed namespace issues, added OptionWrapper

--- Optionally/Option.cs
+++ Optionally/Option.cs
@@ -2,140 +2,135 @@
 
 namespace Optionally
 {
-    using System;
-
-    namespace Optionally
+    public interface IOption<T>
     {
-        public interface IOption<T>
+        /// <summary>
+        /// Convert the current Option to a different Option
+        /// </summary>
+        /// <typeparam name="U">Type to convert to</typeparam>
+        /// <param name="mapper">Function to convert T to U</param>
+        /// <returns>Some if Option is a Some, None otherwise</returns>
+        IOption<U> Map<U>(Func<T, U> mapper);
+
+        /// <summary>
+        /// Chain a function call using the current Option
+        /// </summary>
+        /// <typeparam name="U">Type of Option to return</typeparam>
+        /// <param name="binder">Function to call if Option is Some</param>
+        /// <returns>If Option is Some, returns the result of binder(value), otherwise, None </returns>
+        /// <remarks>Provides a monadic approach to data validation</remarks>
+        IOption<U> AndThen<U>(Func<T, IOption<U>> binder);
+
+        /// <summary>
+        /// Performs an Action on the current Option
+        /// </summary>
+        /// <param name="ifNone">Function to call if Option is None</param>
+        /// <param name="ifSome">Function to call if Option is Some</param>
+        void Do(Action ifNone, Action<T> ifSome);
+
+        /// <summary>
+        /// Check if Option fulfills the filter
+        /// </summary>
+        /// <param name="filter">Predicate used to test Option</param>
+        /// <returns>If Option is Some and the value fulfills the filter, then Some. Otherwise None</returns>
+        IOption<T> Where(Func<T, bool> filter);
+
+        /// <summary>
+        /// Performs a Function on the current Option
+        /// </summary>
+        /// <typeparam name="U">Type to convert to</typeparam>
+        /// <param name="ifNone">Function to call if Option is None</param>
+        /// <param name="ifSome">Function to call if Option is Some</param>
+        /// <returns></returns>
+        U Match<U>(Func<U> ifNone, Func<T, U> ifSome);
+    }
+
+    internal struct Some<T> : IOption<T>
+    {
+        internal readonly T Value;
+
+        public Some(T value)
         {
-            /// <summary>
-            /// Convert the current Option to a different Option
-            /// </summary>
-            /// <typeparam name="U">Type to convert to</typeparam>
-            /// <param name="mapper">Function to convert T to U</param>
-            /// <returns>Some if Option is a Some, None otherwise</returns>
-            IOption<U> Map<U>(Func<T, U> mapper);
-
-            /// <summary>
-            /// Chain a function call using the current Option
-            /// </summary>
-            /// <typeparam name="U">Type of Option to return</typeparam>
-            /// <param name="binder">Function to call if Option is Some</param>
-            /// <returns>If Option is Some, returns the result of binder(value), otherwise, None </returns>
-            /// <remarks>Provides a monadic approach to data validation</remarks>
-            IOption<U> AndThen<U>(Func<T, IOption<U>> binder);
-
-            /// <summary>
-            /// Performs an Action on the current Option
-            /// </summary>
-            /// <param name="ifNone">Function to call if Option is None</param>
-            /// <param name="ifSome">Function to call if Option is Some</param>
-            void Do(Action ifNone, Action<T> ifSome);
-
-            /// <summary>
-            /// Check if Option fulfills the filter
-            /// </summary>
-            /// <param name="filter">Predicate used to test Option</param>
-            /// <returns>If Option is Some and the value fulfills the filter, then Some. Otherwise None</returns>
-            IOption<T> Where(Func<T, bool> filter);
-
-            /// <summary>
-            /// Performs a Function on the current Option
-            /// </summary>
-            /// <typeparam name="U">Type to convert to</typeparam>
-            /// <param name="ifNone">Function to call if Option is None</param>
-            /// <param name="ifSome">Function to call if Option is Some</param>
-            /// <returns></returns>
-            U Match<U>(Func<U> ifNone, Func<T, U> ifSome);
+            Value = value;
+        }
+        public IOption<U> Map<U>(Func<T, U> mapper)
+        {
+            if (mapper == null) throw new ArgumentNullException(nameof(mapper));
+            return new Some<U>(mapper(Value));
         }
 
-        internal struct Some<T> : IOption<T>
+        public IOption<U> AndThen<U>(Func<T, IOption<U>> binder)
         {
-            internal readonly T Value;
-
-            public Some(T value)
-            {
-                Value = value;
-            }
-            public IOption<U> Map<U>(Func<T, U> mapper)
-            {
-                if (mapper == null) throw new ArgumentNullException(nameof(mapper));
-                return new Some<U>(mapper(Value));
-            }
-
-            public IOption<U> AndThen<U>(Func<T, IOption<U>> binder)
-            {
-                if (binder == null) throw new ArgumentNullException(nameof(binder));
-                return binder(Value);
-            }
-
-            public void Do(Action ifNone, Action<T> ifSome)
-            {
-                if (ifSome == null) throw new ArgumentNullException(nameof(ifSome));
-                if (ifNone == null) throw new ArgumentNullException(nameof(ifNone));
-
-                ifSome(Value);
-            }
-
-            public IOption<T> Where(Func<T, bool> filter)
-            {
-                if (filter == null) throw new ArgumentNullException(nameof(filter));
-                return filter(Value) ? (IOption<T>)this : new None<T>();
-            }
-
-            public U Match<U>(Func<U> ifNone, Func<T, U> ifSome)
-            {
-                if (ifSome == null) throw new ArgumentNullException(nameof(ifSome));
-                if (ifNone == null) throw new ArgumentNullException(nameof(ifNone));
-
-                return ifSome(Value);
-            }
-
-            public override string ToString()
-            {
-                return $"Some of '{Value}'";
-            }
+            if (binder == null) throw new ArgumentNullException(nameof(binder));
+            return binder(Value);
         }
 
-        internal struct None<T> : IOption<T>
+        public void Do(Action ifNone, Action<T> ifSome)
         {
-            public IOption<U> Map<U>(Func<T, U> mapper)
-            {
-                if (mapper == null) throw new ArgumentNullException(nameof(mapper));
-                return new None<U>();
-            }
+            if (ifSome == null) throw new ArgumentNullException(nameof(ifSome));
+            if (ifNone == null) throw new ArgumentNullException(nameof(ifNone));
 
-            public IOption<U> AndThen<U>(Func<T, IOption<U>> binder)
-            {
-                if (binder == null) throw new ArgumentNullException(nameof(binder));
-                return new None<U>();
-            }
+            ifSome(Value);
+        }
 
-            public void Do(Action ifNone, Action<T> ifSome)
-            {
-                if (ifSome == null) throw new ArgumentNullException(nameof(ifSome));
-                if (ifNone == null) throw new ArgumentNullException(nameof(ifNone));
-                ifNone();
-            }
+        public IOption<T> Where(Func<T, bool> filter)
+        {
+            if (filter == null) throw new ArgumentNullException(nameof(filter));
+            return filter(Value) ? (IOption<T>)this : new None<T>();
+        }
 
-            public IOption<T> Where(Func<T, bool> filter)
-            {
-                if (filter == null) throw new ArgumentNullException(nameof(filter));
-                return this;
-            }
+        public U Match<U>(Func<U> ifNone, Func<T, U> ifSome)
+        {
+            if (ifSome == null) throw new ArgumentNullException(nameof(ifSome));
+            if (ifNone == null) throw new ArgumentNullException(nameof(ifNone));
 
-            public U Match<U>(Func<U> ifNone, Func<T, U> ifSome)
-            {
-                if (ifSome == null) throw new ArgumentNullException(nameof(ifSome));
-                if (ifNone == null) throw new ArgumentNullException(nameof(ifNone));
+            return ifSome(Value);
+        }
 
-                return ifNone();
-            }
+        public override string ToString()
+        {
+            return $"Some of '{Value}'";
+        }
+    }
 
-            public override string ToString()
-            {
-                return "None";
-            }
+    internal struct None<T> : IOption<T>
+    {
+        public IOption<U> Map<U>(Func<T, U> mapper)
+        {
+            if (mapper == null) throw new ArgumentNullException(nameof(mapper));
+            return new None<U>();
+        }
+
+        public IOption<U> AndThen<U>(Func<T, IOption<U>> binder)
+        {
+            if (binder == null) throw new ArgumentNullException(nameof(binder));
+            return new None<U>();
+        }
+
+        public void Do(Action ifNone, Action<T> ifSome)
+        {
+            if (ifSome == null) throw new ArgumentNullException(nameof(ifSome));
+            if (ifNone == null) throw new ArgumentNullException(nameof(ifNone));
+            ifNone();
+        }
+
+        public IOption<T> Where(Func<T, bool> filter)
+        {
+            if (filter == null) throw new ArgumentNullException(nameof(filter));
+            return this;
+        }
+
+        public U Match<U>(Func<U> ifNone, Func<T, U> ifSome)
+        {
+            if (ifSome == null) throw new ArgumentNullException(nameof(ifSome));
+            if (ifNone == null) throw new ArgumentNullException(nameof(ifNone));
+
+            return ifNone();
+        }
+
+        public override string ToString()
+        {
+            return "None";
         }
     }
 }
---
+++ Optionally/OptionStaticWrapper.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Optionally
+{
+    public static class Option
+    {
+        /// <summary>
+        /// Call a function with Option inputs 
+        /// </summary>
+        /// <typeparam name="T1">Type of the first argument for the function</typeparam>
+        /// <typeparam name="T2">Type of the second argument for the function</typeparam>
+        /// <typeparam name="TResult">Type of the result of the function</typeparam>
+        /// <param name="func"></param>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns>Some(TResult) if first and second are Some. Otherwise, None</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IOption<TResult> Apply<T1, T2, TResult>(Func<T1, T2, TResult> func, IOption<T1> first, IOption<T2> second)
+        {
+            if (func == null) throw new ArgumentNullException(nameof(func));
+            if (first == null) throw new ArgumentNullException(nameof(first));
+            if (second == null) throw new ArgumentNullException(nameof(second));
+
+            var firstIsASome = first is Some<T1>;
+            var secondIsASome = second is Some<T2>;
+
+            if (!firstIsASome || !secondIsASome) return No<TResult>();
+
+            var firstValue = ((Some<T1>)first).Value;
+            var secondValue = ((Some<T2>)second).Value;
+            return Some(func(firstValue, secondValue));
+        }
+
+        /// <summary>
+        /// Call a function with Option inputs
+        /// </summary>
+        /// <typeparam name="T1">Type of the first argument for function</typeparam>
+        /// <typeparam name="T2">Type of the second argument for function</typeparam>
+        /// <typeparam name="T3">Type of the third argument for function</typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="func"></param>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="third"></param>
+        /// <returns>Some(TResult) if first, second, and third are Some. Otherwise, None</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IOption<TResult> Apply<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> func, IOption<T1> first,
+            IOption<T2> second, IOption<T3> third)
+        {
+            if (func == null) throw new ArgumentNullException(nameof(func));
+            if (first == null) throw new ArgumentNullException(nameof(first));
+            if (second == null) throw new ArgumentNullException(nameof(second));
+            if (third == null) throw new ArgumentNullException(nameof(third));
+
+            var firstIsASome = first is Some<T1>;
+            var secondIsASome = second is Some<T2>;
+            var thirdIsASome = third is Some<T3>;
+
+            if (!firstIsASome || !secondIsASome || !thirdIsASome) return No<TResult>();
+
+            var firstValue = ((Some<T1>)first).Value;
+            var secondValue = ((Some<T2>)second).Value;
+            var thirdValue = ((Some<T3>)third).Value;
+            return Some(func(firstValue, secondValue, thirdValue));
+        }
+
+        /// <summary>
+        /// Wrap an exception throwing function into an Option.
+        /// </summary>
+        /// <param name="func"></param>
+        /// <returns>Some(T) if the function succeeds, None otherwise</returns>
+        /// <remarks>Useful for when working with functions that might throw and you want to convert exceptions into None</remarks>
+        public static IOption<T> Wrap<T>(Func<T> func)
+        {
+            try
+            {
+                return Some(func());
+            }
+            catch
+            {
+                return No<T>();
+            }
+        }
+
+        /// <summary>
+        /// Creates an Option with no value inside
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IOption<T> No<T>()
+        {
+            return new None<T>();
+        }
+
+        /// <summary>
+        /// Creates an Option with a value inside
+        /// </summary>
+        /// <typeparam name="T">Type of the value</typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IOption<T> Some<T>(T value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+            return new Some<T>(value);
+        }
+    }
+}
